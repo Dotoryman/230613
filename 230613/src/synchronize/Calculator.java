7e0a68ed71f73da85633110b118113004b2703e7
admin <admin@DESKTOP-63ML1K3>
2023-06-13 13:10:34 +0900

    230613 오전

---
+++ 230613/src/synchronize/Calculator.java
@@ -0,0 +1,9 @@
+package synchronize;
+//User1, User2 작업쓰레드
+public class Calculator {
+	private int memory;
+	
+	public int getMemory() {
+		return memory;
+	}
+}
---
+++ 230613/src/thread/BeepTask.java
@@ -0,0 +1,18 @@
+package thread;
+
+import java.awt.Toolkit;
+
+public class BeepTask implements Runnable {
+
+	public void run() {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		for (int i = 0; i < 5; i++) {
+			toolkit.beep();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
