ffcc48c99327f3f69de071c696f02d7ee4c02798
admin <admin@DESKTOP-63ML1K3>
2023-06-13 09:10:17 +0900

    230613 start

---
+++ 230613/src/phonebook/PhoneBookApp.java
@@ -0,0 +1,15 @@
+package phonebook;
+
+//PhoneInfo: 이름, 연락처
+//PhoneCompanyInfo : 회사 포함
+//PhoneUnivInfo : 학교포함
+//PhoneBookManager : 기능포함
+//MenuChoiceException : 예외사항
+//INIT_MENU, INPUT_SELECT : 메뉴
+//MenuViewer : 메뉴출력
+//추가 조회 삭제
+public class PhoneBookApp {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ 230613/src/phonebook/PhoneInfo.java
@@ -0,0 +1,11 @@
+package phonebook;
+
+public class PhoneInfo {
+	private String name;
+	private String phoneNumber;
+	
+	public PhoneInfo(String name, String PhoneNumber) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+	}
+}
